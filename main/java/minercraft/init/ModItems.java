41042e60721227ec1b4181c6d809ac999ccef9a9
Jack Maloney <jmmaloney4@gmail.com>
2019-08-26 13:21:26 -0500

    Add Copper Ingot

--- main/java/minercraft/init/ModItems.java
+++ main/java/minercraft/init/ModItems.java
@@ -1,8 +1,8 @@
 package minercraft.init;
 
 import minercraft.MinerCraft;
+import minercraft.item.CopperIngot;
 import minercraft.item.TungstenIngot;
-
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -14,20 +14,24 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber(modid=MinerCraft.MODID)
 public class ModItems {
 	
+	static Item copperIngot;
 	static Item tungstenIngot;
 
 	public static void init() {
 		tungstenIngot = new TungstenIngot();
+		copperIngot = new CopperIngot();
 	}
 
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(tungstenIngot);
+		event.getRegistry().registerAll(copperIngot);
 	}
 	
 	@SubscribeEvent
 	public static void registerRenders(ModelRegistryEvent event) {
 		registerRender(tungstenIngot);
+		registerRender(copperIngot);
 	}
 	
 	private static void registerRender(Item item) {
---
+++ main/java/minercraft/item/CopperIngot.java
@@ -0,0 +1,15 @@
+package minercraft.item;
+
+import net.minecraft.item.Item;
+
+public class CopperIngot extends Item {
+	
+	private static String NAME = "copper_ingot";
+
+	public CopperIngot() {
+		setUnlocalizedName(CopperIngot.NAME);
+		setRegistryName(CopperIngot.NAME);
+	}
+}
+
+
